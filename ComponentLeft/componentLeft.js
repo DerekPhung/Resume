190fb1e2db065ddb2ab3e5445f956e515a8f8c46
FluffyLinux <fluffywin@gmail.com>
2022-11-22 02:27:29 -0500

    added color

--- ComponentLeft/componentLeft.js
+++ ComponentLeft/componentLeft.js
@@ -1,13 +1,31 @@
 
 //skills education 'work experience' projects 'major courses' 'award and achievement' 'Extracurricular Activities'
-let navList = ["Education","Skills and Technique", "Work Experience", "Projects", "Major Courses", "Awards and Achievements", "Extracurricular Activities", "Top"];
-let navID = ["education","skills","experience","projects","courses", "awards","activities","Top"];
-document.getElementById("bigBox").innerHTML += `
+let navList = [
+  "Skills", "Education", "Work Experience", 
+  "Projects", "Major Courses", "Awards and Achievements", 
+  "Extracurricular Activities", "Top"
+];
+
+let navID = [
+  "skills","education","experience","projects",
+"courses", "awards","activities","Top"
+];
+
+
+document.getElementById("bigBox").innerHTML += 
+`
   <div id="columnLeft">
 
   </div>
 `;
+
 for(let i = 0; i < navList.length; i++){
-  document.getElementById("columnLeft").innerHTML += `
-  <div id="${navID[i]}" class="navBox">${navList[i]}</div>`;
+  document.getElementById("columnLeft").innerHTML += 
+  `
+    <div class="navBox">
+      <a href="#${navID[i]}">
+        ${navList[i]}
+      </a>
+    </div>
+  `;
 }
