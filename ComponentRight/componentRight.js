3ff6dff91c836e29d95b0c8c42440b2ca09ef299
Joeywin42 <joeywin42@gmail.com>
2022-11-21 20:33:33 -0500

    component right

--- ComponentRight/componentRight.js
+++ ComponentRight/componentRight.js
@@ -1,15 +1,29 @@
 
 
 function bodyText(){
+    document.getElementById("columnRight").innerHTML += 
+    `testing <br/> <br/>`;
 
 }
 
 function main(){
-    document.getElementById("bigBox").innerHTML = `
-  <div id="columnRight">
-    
-  </div>
-`;
+    document.getElementById("bigBox").innerHTML += `
+    <div id="columnRight">
+      
+    </div>
+  `;
+
+  bodyText();
+
+  for(let i = 0; i < 5; i++){
+    bodyText();
+  }
+
+
+
+
+
+
 
 }
 
