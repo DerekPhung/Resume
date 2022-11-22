1a48656746ae7445c7383c8b7649a9bf180671fe
Joeywin42 <joeywin42@gmail.com>
2022-11-21 21:42:50 -0500

    more funciton

--- ComponentRight/componentRight.js
+++ ComponentRight/componentRight.js
@@ -1,30 +1,37 @@
 
 
 function bodyText(topic, description){
+    console.log("Running bodyText")
+    //print body text method
     document.getElementById("columnRight").innerHTML += 
-    `${topic} <br/>
-    ${description} <br/> <br/> <br/>`;
+    `<div class="headerRight"> ${topic}</div>
+    <p1> ${description} <br/>
+    <br/> <br/> </p1>
+    `;
 
 }
-
+//main
 function main(){
+    console.log("Running Main of componentRight.js")
     document.getElementById("bigBox").innerHTML += `
     <div id="columnRight">
       
     </div>
   `;
 
-  bodyText("test", "testing" );
 
-  for(let i = 0; i < 5; i++){
-    bodyText("test" + i, "testing");
+  //array of information
+  let topics = ["test1" ,"test2" ,"test3" ,"test4"]
+  let descriptions = ["test1", "test2", "test3", "test4"];
+
+  //print out information in body text format
+  for(let i = 0; i < topics.length; i++){
+    bodyText(topics[i], descriptions[i]);
   }
 
 
-
-
 }
-
+//Running
 console.log("Component Right is running")
-
 main();
+console.log("Component Right has ran correctly")
