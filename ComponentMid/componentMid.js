d1a819cb7c6fcbaa631f24eeb2afe9105e5e9ff8
TubbieTubTub <derekphung16@gmail.com>
2022-11-21 22:15:13 -0500

    Added more content in the main column

--- ComponentMid/componentMid.js
+++ ComponentMid/componentMid.js
@@ -1,19 +1,25 @@
-document.getElementById("bigBox").innerHTML += `
+const newLocal = document.getElementById("bigBox").innerHTML += `
 <div id="columnMid">
 
-<h1 class="title">Education</h1>
+    <h1 class="title">Skills</h1>
     <ul>
-        <li>Graduated Berkmar High School in 2010</li>
-        <li>Enrolled at Gerogia Gwinnett College in 2018</li>
+        <li>Microsoft Suite</li>
+        <li>SharePoint</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Java</li>
+        <li>Python</li>
+        <li>SQL</li>
+        <li>Git</li>
+        <li>PHP</li>
+        <li>Waterfall</li>
+        <li>Agile</li>
     </ul>
 
-    <h1 class="title">Skills and Techniques</h1>
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
+    <h1 class="title">Education</h1>
+
+    <h2>Bachelors of Science</h2>
 
     <h1 class="title">Work Experience</h1>
     <ul>
@@ -23,14 +29,19 @@ document.getElementById("bigBox").innerHTML += `
 
     <h1 class="title">Projects</h1>
     <ul>
-        <li>Graduated Berkmar High School in 2010</li>
-        <li>Enrolled at Gerogia Gwinnett College in 2018</li>
+        <li>Created a Rock, Paper, Scissors game using Java</li>
+        <li>Candy Catching game built through <a href="https://scratch.mit.edu/projects/481519033">Scratch</a></li>
+        <li><a href="http://dphung.altervista.org/index.html">My website</a></li>
+
     </ul>
 
     <h1 class="title">Major Courses</h1>
     <ul>
-        <li>Graduated Berkmar High School in 2010</li>
-        <li>Enrolled at Gerogia Gwinnett College in 2018</li>
+        <li>Web Tech</li>
+        <li>Programming Fundamentals</li>
+        <li>Intermediate Programming</li>
+        <li>Computer Graphics and Multimedia</li>
+        <li>Technology Ambassador Program</li>
     </ul>
 
     <h1 class="title">Awards and Achievements</h1>
