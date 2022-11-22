0e79acd542a4ac455d27ee49a9970169eeaaa9bb
TubbieTubTub <derekphung16@gmail.com>
2022-11-21 20:11:29 -0500

    I add the main content and also some minimal css

---
+++ ComponentMid/componentMid.js
@@ -0,0 +1,47 @@
+document.getElementById("bigBox").innerHTML = `
+<div id="columnMid">
+
+<h1 class="title">Education</h1>
+    <ul>
+        <li>Graduated Berkmar High School in 2010</li>
+        <li>Enrolled at Gerogia Gwinnett College in 2018</li>
+    </ul>
+
+    <h1 class="title">Skills and Techniques</h1>
+    <ul>
+        <li>Graduated Berkmar High School in 2010</li>
+        <li>Enrolled at Gerogia Gwinnett College in 2018</li>
+    </ul>
+
+    <h1 class="title">Work Experience</h1>
+    <ul>
+        <li>Graduated Berkmar High School in 2010</li>
+        <li>Enrolled at Gerogia Gwinnett College in 2018</li>
+    </ul>
+
+    <h1 class="title">Projects</h1>
+    <ul>
+        <li>Graduated Berkmar High School in 2010</li>
+        <li>Enrolled at Gerogia Gwinnett College in 2018</li>
+    </ul>
+
+    <h1 class="title">Major Courses</h1>
+    <ul>
+        <li>Graduated Berkmar High School in 2010</li>
+        <li>Enrolled at Gerogia Gwinnett College in 2018</li>
+    </ul>
+
+    <h1 class="title">Awards and Achievements</h1>
+    <ul>
+        <li>Graduated Berkmar High School in 2010</li>
+        <li>Enrolled at Gerogia Gwinnett College in 2018</li>
+    </ul>
+
+    <h1 class="title">Extracurricular Activities</h1>
+    <ul>
+        <li>Graduated Berkmar High School in 2010</li>
+        <li>Enrolled at Gerogia Gwinnett College in 2018</li>
+    </ul>
+  
+</div>
+`;
